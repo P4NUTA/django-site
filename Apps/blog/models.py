71929181131b07b1cdb147b16a763dd10ebd92e6
Павел <PaulEfimov2001@yandex.ru>
2020-07-23 23:36:10 +0300

    prepare to html

--- Apps/blog/models.py
+++ Apps/blog/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.urls import reverse
 from django.utils import timezone
 
 
@@ -25,6 +26,12 @@ class Post(models.Model):
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
     objects = models.Manager()
     published = PublishedManager()
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',
+                       args=[self.publish.year,
+                             self.publish.month,
+                             self.publish.day, self.slug])
 
     class Meta:
         ordering = ('-publish',)
